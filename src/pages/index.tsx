480ac2c276ec2354a3bbcce0d042a7726320fa0d
habirov94 <habirov-94@mail.ru>
2024-05-27 22:43:16 +0500

    [27.05.2024] Рефакторинг кода, создает страницу для актеров

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
+import React from "react";
 import {IRoutes} from "shared/types";
-import {MainPage} from "pages/main-page";
+import {MainPage} from "./main-page";
+import {personsPage} from "./persons-page";
 
 export const routes:IRoutes[] = [
     {
         name: "Главная",
         index: true,
-        element: <MainPage />,
-        component: <MainPage />
+        element: React.createElement(MainPage),
+        component: React.createElement(MainPage)
     },
     {
         name: "Главная",
         path: "/main",
         isVisible: true,
-        component: <MainPage />
-    }
+        component: React.createElement(MainPage)
+    },
+    {
+        name: "Актеры",
+        path: "/persons",
+        isVisible: true,
+        component: React.createElement(personsPage)
+    },
 ]
---
+++ src/pages/persons-page/index.tsx
@@ -0,0 +1,7 @@
+export const personsPage = () => {
+    return (
+        <div>
+            123
+        </div>
+    )
+}
